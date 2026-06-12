792a99834348a3ee26eb4f5e60b556741d2972dc
Hureru <3507039083@qq.com>
2026-06-12 13:56:33 +0800

    refactor: keep API key card at top of setting page

--- web/src/components/modules/setting/index.tsx
+++ web/src/components/modules/setting/index.tsx
@@ -14,10 +14,10 @@ export function Setting() {
     return (
         <div className="h-full min-h-0 overflow-y-auto overscroll-contain rounded-t-3xl">
             <PageWrapper className="columns-1 gap-4 pb-24 md:columns-2 md:pb-4 *:mb-4 *:min-w-0 *:break-inside-avoid">
+                <SettingAPIKey key="setting-apikey" />
                 <SettingInfo key="setting-info" />
                 <SettingAppearance key="setting-appearance" />
                 <SettingAccount key="setting-account" />
-                <SettingAPIKey key="setting-apikey" />
                 <SettingNetwork key="setting-network" />
                 <SettingReliability key="setting-reliability" />
                 <SettingSyncTasks key="setting-sync-tasks" />
